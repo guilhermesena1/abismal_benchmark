b2765ad6406eca36dce106e27f7fa8559af5a44d
guilhermesena1 <desenabr@usc.edu>
2021-08-27 12:18:21 -0700

    adding simulation accuracy data

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+# run snakemake script on a SLURM machine
 snakemake --printshellcmds --keep-going --ignore-incomplete \
           --cluster-config cluster.yaml \
-          --latency-wait 300 --cluster \
-          "sbatch --partition=cmb --time=100:00:00 --mem={cluster.mem} \
+          --latency-wait 60 --cluster \
+          "sbatch --partition=qcb --time=200:00:00 --mem={cluster.mem} \
           --ntasks={cluster.threads} \
           --ntasks-per-node={cluster.threads} \
           --job-name=sm_{cluster.name} \
